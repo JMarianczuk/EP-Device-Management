7e6f7bbfa73a781f153dd1f8a16138b7a2582559
Jasper Marianczuk <jasper.marianczuk@gmail.com>
2022-08-18 15:46:07 +0200

    add new results with repaired data

--- result/plot.r
+++ result/plot.r
@@ -3,21 +3,38 @@ library(optparse)
 library(RSQLite)
 
 con <- dbConnect(SQLite(), "results.sqlite")
-someRes <- dbExecute(con, "PRAGMA case_sensitive_like=ON;")
+some_res <- dbExecute(con, "PRAGMA case_sensitive_like=ON;")
 
-query <- paste("select", "*, count(*) as size", 'from simulation where success="True" group by strategy, probability, packetSize')
-
-print(query)
-
-res <- dbGetQuery(con, query)
-numValue <- function(text) {
-    onlyNumber <- substring(text, first=1, last=nchar(text) - 2)
-    numeric <- as.numeric(onlyNumber)
-    numeric
+get_query <- function(data_name) {
+    data_where <- paste('data="', data_name, '"', sep="")
+    query <- paste(
+        "select",
+        "*, count(*) as size",
+        "from simulation",
+        'where success="True" and', data_where,
+        "group by strategy, probability, packetSize")
+    query
 }
 
-thisplot <- ggplot(res, aes(x = reorder(probability, numValue(probability)), y = packetSize, colour=strategy, size=size)) +
-    geom_jitter() +
-    labs(x = "probability")
+do_plot <- function(data_name, title, file_name) {
+    res <- dbGetQuery(con, get_query(data_name))
+    num_value <- function(text) {
+        only_number <- substring(text, first = 1, last = nchar(text) - 2)
+        numeric <- as.numeric(only_number)
+        numeric
+    }
 
-ggsave("plot.pdf", thisplot, width=25, height=10, units="cm")
+    thisplot <- ggplot(res, aes(
+        x = reorder(probability, num_value(probability)),
+        y = packetSize,
+        colour = strategy,
+        size = size)
+        ) +
+        geom_jitter() +
+        labs(x = "probability", title = title)
+
+    ggsave(file_name, thisplot, width=25, height=10, units="cm")
+}
+
+do_plot("Res1", "simulating residential 1", "plot_res1.pdf")
+do_plot("Res1 noSolar", "simulating residential 1 without solar", "plot_res1_noSolar.pdf")
